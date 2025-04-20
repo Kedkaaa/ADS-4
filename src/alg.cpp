a33c4524b984544df310c6e931fdce932114ec07
Kuchumova_Ekaterina <e_kuchumova_06@mail.ru>
2025-04-20 16:49:56 +0300

    Commit_1.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,70 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        ++count;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int left = 0;
+  int right = len - 1;
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      int leftVal = arr[left];
+      int rightVal = arr[right];
+      int leftCount = 0;
+      while (left < right && arr[left] == leftVal) {
+        ++leftCount;
+        ++left;
+      }
+      int rightCount = 0;
+      while (right >= left && arr[right] == rightVal) {
+        ++rightCount;
+        --right;
+      }
+      count += leftCount * rightCount;
+    }
+    else if (sum < value) {
+      ++left;
+    }
+    else {
+      --right;
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    int target = value - arr[i];
+    int left = i + 1;
+    int right = len - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (arr[mid] < target)
+        left = mid + 1;
+      else
+        right = mid - 1;
+    }
+    int lower = left;
+    left = i + 1, right = len - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (arr[mid] <= target)
+        left = mid + 1;
+      else
+        right = mid - 1;
+    }
+    int upper = right;
+    if (lower <= upper)
+      count += (upper - lower + 1);
+  }
+  return count;
 }
