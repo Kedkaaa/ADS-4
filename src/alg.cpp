fd03d173eba516320225ff50b846ebc9e610925b
Kuchumova_Ekaterina <e_kuchumova_06@mail.ru>
2025-05-01 15:53:03 +0300

    Commit_16.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,23 +17,13 @@ int countPairs2(int *arr, int len, int value) {
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
-      if (arr[left] == arr[right]) {
-        int n = right - left + 1;
-        count += (n * (n - 1)) / 2;
-        break;
-      } else {
-          int leftVal = arr[left], leftCount = 0;
-          while (left < right && arr[left] == leftVal) {
-            ++leftCount;
-            ++left;
-          }
-          int rightVal = arr[right], rightCount = 0;
-          while (right >= left && arr[right] == rightVal) {
-            ++rightCount;
-            --right;
-          }
-          count += leftCount * rightCount;
-        }
+      int Left = left;
+      int Right = right;
+      while (Left < Right && arr[Left] + arr[Right] == value) {
+        ++count;
+        ++Left;
+      }
+      --right;
     } else if (sum < value) {
       ++left;
     } else {
